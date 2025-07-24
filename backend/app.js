9e42a6c33c58b88df5b9021548bc91c0885be6ec
Subham Sahu <subhambusiness286@gmail.com>
2025-07-24 21:20:29 +0530

    Ready to deploy

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,12 @@ const sqlite = require('sqlite3').verbose();
 const path = require('path');
 
 const app = express();
-const PORT = 1000;
+const PORT = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
 
 const db = new sqlite.Database('./database.db');
-
-app.use(express.static(path.join(__dirname, 'frontend', 'build')));
-
 
 app.get("/stocks", (req, res) => {
   db.all("SELECT DISTINCT symbol FROM stocks", [], (err, rows) => {
@@ -29,10 +26,15 @@ app.get("/stocks/:symbol", (req, res) => {
   });
 });
 
+app.use(express.static(path.join(__dirname, 'frontend', 'build')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-app.listen(PORT, "0.0.0.0", () => {
+const server = app.listen(PORT, "0.0.0.0", () => {
     console.log(`Started on port ${PORT}`)
-})
+});
+
+server.keepAliveTimeout = 120000;
+server.headersTimeout = 120000;
